a238374fbba76b718a83fff4692d655716f2e5fa
cybios <cybios@126.com>
2017-10-15 20:01:52 +0800

    Init

---
+++ TextDemo/TextDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TextDemo
+//
+//  Created by test on 2017/10/15.
+//  Copyright © 2017年 test. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ TextDemo/TextDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TextDemo
+//
+//  Created by test on 2017/10/15.
+//  Copyright © 2017年 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
